724390f6272c27d688b1f66e3f7a218b967873ae
heeren17 <miyukichan003@gmail.com>
2023-03-20 10:19:17 +0700

    Initial commit

---
+++ app/src/main/java/com/M/Fikri/R/P_202102250/emailform/MainActivity.java
@@ -0,0 +1,14 @@
+package com.M.Fikri.R.P_202102250.emailform;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
